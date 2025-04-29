c7ab04a5a35eeb29549a9e3e9751badf44aaefc4
Miguel Carrizo <miguel.carrizo@externos-cl.cencosud.com>
2025-04-29 14:56:54 -0400

    create setu.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 1. Create .env at /
+cat > .env <<EOF
+DATABASE_URL=postgresql://admin:g3smartjob@localhost:5432/g3stmart
+EOF
+
+# 2. Create .env at database/docker
+cat > database/docker/.env <<EOF
+POSTGRES_USER=admin
+POSTGRES_PASSWORD=g3smartjob
+POSTGRES_DB=g3stmart
+EOF
+
+# 3. Start database
+cd database/docker
+docker compose --env-file .env -p g3stmart up -d
+
+# 4. Install deps & generate Prisma client
+cd ../..
+npm install
+npx prisma generate
+
+# 5. Run dev server
+npm run dev 
