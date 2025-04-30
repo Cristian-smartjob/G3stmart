c21998f142a8e88c9d2172d338de9225d18158ac
Miguel Carrizo <miguel.carrizo@externos-cl.cencosud.com>
2025-04-29 21:35:43 -0400

    set username to Usuario

---
+++ setup.bat
@@ -0,0 +1,21 @@
+@echo off
+
+REM 1. Create .env at /
+echo DATABASE_URL=postgresql://admin:g3smartjob@localhost:5432/g3stmart > .env
+
+REM 2. Create .env at database/docker
+echo POSTGRES_USER=admin > database\docker\.env
+echo POSTGRES_PASSWORD=g3smartjob >> database\docker\.env
+echo POSTGRES_DB=g3stmart >> database\docker\.env
+
+REM 3. Start database
+cd database\docker
+docker compose --env-file .env -p g3stmart up -d
+
+REM 4. Install deps & generate Prisma client
+cd ..\..
+call npm install
+call npx prisma generate
+
+REM 5. Run dev server
+call npm run dev 
